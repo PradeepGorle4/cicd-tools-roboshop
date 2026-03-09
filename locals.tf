3b4889a298087ada376b3a5f34d1fb685d6b68c6
PradeepGorle4 <pradeepgorle4@gmail.com>
2026-03-09 17:57:04 +0530

    Roboshop-cicd

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals{
-    # ami_id = data.aws_ami.joindevops.id
+    ami_id = data.aws_ami.joindevops.id
     # sonar_ami_id = data.aws_ami.sonarqube.id
     common_tags = {
         Project = var.project
--- variables.tf
+++ variables.tf
@@ -24,5 +24,5 @@ variable "zone_id" {
 
 variable "ami_id" {
    type  = string
-   default = "ami-09c813fb71547fc4f"
+   default = "ami-0220d79f3f480ecf5"
 }
